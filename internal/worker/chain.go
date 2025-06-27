1ca57178339e251d80005dd3a4f2bc5ed60d169e
Mohammed Sohail <sohailsameja@gmail.com>
2025-06-27 13:54:26 +0300

    feat: add token deploy worker

--- internal/worker/chain.go
+++ internal/worker/chain.go
@@ -3,25 +3,27 @@ package worker
 import "github.com/lmittmann/w3"
 
 const (
-	Approve      = "approve"
-	Check        = "check"
-	GiveTo       = "giveTo"
-	MintTo       = "mintTo"
-	NextTime     = "nextTime"
-	Register     = "register"
-	Transfer     = "transfer"
-	TransferFrom = "transferFrom"
-	Withdraw     = "withdraw"
-	Deposit      = "deposit"
-	Sweep        = "sweep"
+	Approve           = "approve"
+	Check             = "check"
+	GiveTo            = "giveTo"
+	MintTo            = "mintTo"
+	NextTime          = "nextTime"
+	Register          = "register"
+	Transfer          = "transfer"
+	TransferFrom      = "transferFrom"
+	TransferOwnership = "transferOwnership"
+	Withdraw          = "withdraw"
+	Deposit           = "deposit"
+	Sweep             = "sweep"
 )
 
 var abi = map[string]*w3.Func{
 	// ERC20
-	Approve:  w3.MustNewFunc("approve(address, uint256)", "bool"),
-	MintTo:   w3.MustNewFunc("mintTo(address, uint256)", "bool"),
-	Transfer: w3.MustNewFunc("transfer(address,uint256)", "bool"),
-	Sweep:    w3.MustNewFunc("sweep(address)", "uint256"),
+	Approve:           w3.MustNewFunc("approve(address, uint256)", "bool"),
+	MintTo:            w3.MustNewFunc("mintTo(address, uint256)", "bool"),
+	Transfer:          w3.MustNewFunc("transfer(address,uint256)", "bool"),
+	Sweep:             w3.MustNewFunc("sweep(address)", "uint256"),
+	TransferOwnership: w3.MustNewFunc("transferOwnership(address)", "bool"),
 	// GasFaucet
 	Check:    w3.MustNewFunc("check(address)", "bool"),
 	GiveTo:   w3.MustNewFunc("giveTo(address)", "uint256"),
